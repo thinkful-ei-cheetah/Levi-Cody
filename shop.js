8e4dc8fb651f710948f2f091304d46fbd6f0f758
Levi Paulk <levipaulk@gmail.com>
2019-03-28 16:28:25 -0400

    start js

--- shop.js
+++ shop.js
@@ -1,8 +1,26 @@
+'use strict';
 //add new item to shopping list
-
+function newItem() {
+    $("button").on('click', function(event) {
+        console.log(event)
+    //   $("ul").append(
+    //     `<li>
+    //     <span class="shopping-item">TEST!!!</span>
+    //     <div class="shopping-item-controls">
+    //       <button class="shopping-item-toggle">
+    //         <span class="button-label">check</span>
+    //       </button>
+    //       <button class="shopping-item-delete">
+    //         <span class="button-label">delete</span>
+    //       </button>
+    //     </div>
+    //   </li>`
+    //   );
+    })
+}
 //toggle item status
 
 //delete item
 
 
-$();
+$(newItem());
