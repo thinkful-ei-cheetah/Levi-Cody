21b5681aac1c6837c40e19e02da19d5628195558
Cody Gillette <cgillette1312@gmail.com>
2019-03-28 14:20:49 -0700

    shop

--- shop.js
+++ shop.js
@@ -1,21 +1,25 @@
 'use strict';
 //add new item to shopping list
 function newItem() {
-    $("button").on('click', function(event) {
-        console.log(event)
-    //   $("ul").append(
-    //     `<li>
-    //     <span class="shopping-item">TEST!!!</span>
-    //     <div class="shopping-item-controls">
-    //       <button class="shopping-item-toggle">
-    //         <span class="button-label">check</span>
-    //       </button>
-    //       <button class="shopping-item-delete">
-    //         <span class="button-label">delete</span>
-    //       </button>
-    //     </div>
-    //   </li>`
-    //   );
+    $("#js-shopping-list-form").submit( function(event) {  
+        event.preventDefault();
+        let newItem = $(event.currentTarget).find('#shopping-list-entry');
+    //    if( typeof $('shopping-list-entry') === 'string'){
+    //        throw new Error('need this please')
+    //    }
+      $("ul").append(
+        `<li>
+        <span class="shopping-item">${newItem.val()}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+      );
     })
 }
 //toggle item status
@@ -24,4 +28,4 @@ function newItem() {
 
 
 $(newItem());
-console.log('hello world')
+// console.log('hello world')
